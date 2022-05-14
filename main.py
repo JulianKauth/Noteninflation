355f7fe8f73f476e07c735ccf9bc411a7e515d63
Julian Kauth <julian.kauth@fau.de>
2022-05-14 22:00:13 +0200

    Playing with plot 1

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 
+import matplotlib.pyplot as plt
 import pandas as pd
 import openpyxl
 
@@ -13,7 +14,7 @@ def load_data() -> pd.DataFrame:
     if os.path.exists(pickle_file):
         return pd.read_pickle(pickle_file)
 
-    columns = ["Year", "State", "Participants", "Passed", "Failed", "Grade", "Count"]
+    columns = ["Year", "State", "Participants", "Passed", "Failed", "FailedPercent", "AvgGrade", "Grade", "Count"]
 
     rows = []  # cols in the Excel sheet, but let's move to the dataframe state of mind as soon as possible
     for year in range(2006, 2021 + 1):
@@ -22,7 +23,7 @@ def load_data() -> pd.DataFrame:
         ws = openpyxl.load_workbook(filename=filename, read_only=True)["Noten"]
         for col in "BCDEFGHIJKLMNOPQ":
             row = [year, ws[col + "5"].value]
-            row.extend([int(cell[0].value) for cell in ws[f"{col}6:{col}8"]])
+            row.extend([cell[0].value for cell in ws[f"{col}6:{col}10"]])
             grade_counts = [int(cell[0].value) for cell in ws[f"{col}12:{col}42"]]
             for i, count in enumerate(grade_counts):
                 grade = (i + 10) / 10
@@ -33,6 +34,33 @@ def load_data() -> pd.DataFrame:
     return re
 
 
-if __name__ == '__main__':
-    df = load_data()
+def simple_plot(df, kind, x, y, title="Test"):
     print(df)
+    df.reset_index().plot(kind=kind, x=x, y=y)
+    plt.title(title)
+    plt.show()
+
+
+def plot(df):
+    kind = "line"
+    x = "Year"
+    y = "AvgGrade"
+    title = "Test"
+
+    ax = plt.gca()
+    for state, state_df in df.groupby("State"):
+        state_df.groupby(["Year"]).mean().reset_index().plot(kind=kind, x=x, y=y, ax=ax)
+
+    plt.title(title)
+    plt.show()
+
+
+if __name__ == '__main__':
+    dataframe = load_data()
+
+    # simple_plot(dataframe.groupby(["Year"]).sum().reset_index(), "bar", "Year", "Participants")
+    # simple_plot(dataframe.groupby(["Grade", "Year"]).sum(), "bar", "Grade", "Count")
+    # simple_plot(dataframe.groupby(["Year"]).mean(), "line", "Year", "AvgGrade")
+    # simple_plot(dataframe.groupby(["Year"]).mean(), "line", "Year", "FailedPercent")
+    # simple_plot(dataframe[dataframe.State == "BY"].groupby(["Year"]).mean(), "line", "Year", "AvgGrade")
+    plot(dataframe)
